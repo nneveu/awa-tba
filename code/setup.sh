f25ce61891ac0d58f053eef9395788e0c4a0ea1e
Nicole Neveu <nneveu@hawk.iit.edu>
2018-03-05 18:28:26 -0600

    input generation # changes

--- code/setup.sh
+++ code/setup.sh
@@ -1,6 +1,8 @@
 # general of opal
-export TIME=00:30:00
+rm -rf results *.0 *_0
+rm -rf tmp
+mkdir tmp results
 export QUEUE=knlall
 export TEMPLATES=`pwd`/tmpl/
 export FIELDMAPS=/lcrc/project/AWA-beam-dynamics/FieldFiles/DriveFiles
-export OPAL_EXE_PATH=/home/neveu/software/opal-1.9/knl/bin
+export OPAL_EXE_PATH=/lcrc/project/AWA-beam-dynamics/software/opal/opal-1.9/build-knl/src
