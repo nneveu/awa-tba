49a642b8f87b5d0c0845e7ff76c52368f02cc7ec
nneveu <nneveu@hawk.iit.edu>
2018-04-05 12:16:39 -0500

    trying to make new repo

---
+++ doVis.sh
@@ -0,0 +1,11 @@
+#
+# visualize.py comes with pyOPALTools
+#
+# usage: visualize.py Gen150_Pop647 10
+#
+#
+visualize.py \
+--objectives=emitx2,rmss2,rmss2 \
+--path=$1 \
+--generation=$2 \
+--filename-postfix=optLinac_40nC.dat_0
