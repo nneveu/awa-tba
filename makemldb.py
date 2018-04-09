d3e4c4ac3d92443fe220560aa518f29a8628fc83
Nicole Neveu <nneveu@hawk.iit.edu>
2018-04-08 23:58:36 -0500

    making db for first paper run

--- makemldb.py
+++ makemldb.py
@@ -1,7 +1,7 @@
 import sys, os
-#sys.path.insert(0, '/lcrc/project/AWA-beam-dynamics/software/pyOPALTools/')
-#sys.path.insert(0, '/lcrc/project/AWA-beam-dynamics/software/pyOPALTools/db/')
-#sys.path.insert(0, '/lcrc/project/AWA-beam-dynamics/software/pyOPALTools/utilities/')
+sys.path.insert(0, '/lcrc/project/AWA-beam-dynamics/software/pyOPALTools/')
+sys.path.insert(0, '/lcrc/project/AWA-beam-dynamics/software/pyOPALTools/db/')
+sys.path.insert(0, '/lcrc/project/AWA-beam-dynamics/software/pyOPALTools/utilities/')
 from db import mldb
 ''' 
 We create a Pickle file from a number of OPAL 
@@ -16,26 +16,25 @@ The content of the file is
 #baseFN = 'rand_sample'
 #root   = "/lcrc/project/AWA-beam-dynamics/surrogatemodels/random_sample/surrogate_models_rand_sample_bounds2"
 
-baseFN = ['ex-1', 'ex-2', 'ex-3', 'ex-4']
-#root   = "/lcrc/project/AWA-beam-dynamics/surrogatemodels/ga-model/results"
+baseFN = 'paper-run-1' #['ex-1', 'ex-2', 'ex-3', 'ex-4']
+root   = "/lcrc/project/AWA-beam-dynamics/Nicole/awa-tba/paper-run-1/results"
 #root   = "./ex-4/results"
 
-#baseFN = 'paper-pc'
-#root   = "/lcrc/project/AWA-beam-dynamics/surrogatemodels/pc-models/paper-pc-model/data/"
-
 #yNames = ['s','numParticles','rms_x','rms_y','rms_s','emit_x','emit_y','emit_s','energy','dE']
-#dbw    = mldb.mldb()
+dbw    = mldb.mldb()
 
 #Build from stat files
 #dbw.buildFromSDDS(baseFN, root, yNames)
 #dbw.printOverview()
 
 #Build from json files
-#dbw.build(baseFN, root)
-#dbw.printOverview()
+dbw.build(baseFN, root)
+dbw.printOverview()
 
 #Make bounded db
-for ex in baseFN:
-    pickle = ex+'.pk'
-    mldb.buildBounded(pickle, ex)
+#for ex in baseFN:
+#    pickle = ex+'.pk'
+#    mldb.buildBounded(pickle, ex)
+pickle = baseFN+'.pk'
+mldb.buildBounded(pickle, baseFN)
 
