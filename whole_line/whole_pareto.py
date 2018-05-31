523f5fc8a6b427b61b99d1ab659b98d0a1c5bb60
nicole <nneveu@hawk.iit.edu>
2018-05-30 19:52:28 -0500

    loading data

--- whole_line/whole_pareto.py
+++ whole_line/whole_pareto.py
@@ -16,16 +16,29 @@ plt.rc('font', family='serif')
 
 root = '/home/nicole/Documents/awa-tba/'
 
-baseFN = ['optLinac_40nC']
+#baseFN = ['./noquads']#['optLinac_40nC']
+#baseFN = ['./noquads/corrected/']
+baseFN = ['./extquads']
 
 for fn in baseFN:
-    allemx1 = []
+    allemx1  = []
     allrmss1 = []
     allrmsx1 = []
-  
-    allemx2 = []
+    allde1   = [] 
+
+    allemx2  = []
     allrmss2 = []
     allrmsx2 = []
+
+    allde3   = []
+    allemx3  = []
+    allrmss3 = []
+    allrmsx3 = []
+   
+    allemx4  = []
+    allrmss4 = []
+    allrmsx4 = []
+     
     allq1   = []
     allq2   = [] 
     allq3   = []
@@ -36,16 +49,26 @@ for fn in baseFN:
     ds    = dsets[0]
 
     for i in range(1,ngen+1):
-
+        #Loadig all data in the i-th generation
         de    = ds.getData('DE1', gen=i)
         emx1  = ds.getData('EMITX1', gen=i)
-        emx2  = ds.getData('EMITX2', gen=i)
         rmss1 = ds.getData('RMSS1', gen=i)
-        rmss2 = ds.getData('RMSS2', gen=i)
         rmsx1 = ds.getData('RMSX1', gen=i)
+        
+        rmss2 = ds.getData('RMSS2', gen=i)
         rmsx2 = ds.getData('RMSX2', gen=i)
- 
-    
+        emx2  = ds.getData('EMITX2', gen=i)
+
+        de3    = ds.getData('DE3', gen=i)
+        emx3  = ds.getData('EMITX3', gen=i)
+        rmss3 = ds.getData('RMSS3', gen=i)
+        rmsx3 = ds.getData('RMSX3', gen=i)
+
+        emx4  = ds.getData('EMITX4', gen=i)
+        rmss4 = ds.getData('RMSS4', gen=i)
+        rmsx4 = ds.getData('RMSX4', gen=i)
+        #rmsy4 = ds.getData('RMSY4', gen=i)
+
         gphs = ds.getData('GPHASE', gen=i)
         fwhm = ds.getData('FWHM', gen=i)
         im = ds.getData('IM', gen=i)
@@ -54,7 +77,24 @@ for fn in baseFN:
         q2 = ds.getData('KQ2', gen=i)
         q3 = ds.getData('KQ3', gen=i)
         q4 = ds.getData('KQ4', gen=i)
+        #Some runs don't have q5 and q6
+        try:
+            q5 = ds.getData('KQ5', gen=i)
+            q6 = ds.getData('KQ6', gen=i)
+        except:
+            pass
+            #print('no q5, q6')
+       
+        try: 
+            q7 = ds.getData('KQ7', gen=i)
+            q8 = ds.getData('KQ8', gen=i)
+            q9 = ds.getData('KQ9', gen=i)
+        except:
+            pass
+            #print('no q7-q9')
 
+        #Saving i-th generation data to an array
+        #that holds data for every generation
         allemx1  = np.append(emx1, allemx1)
         allrmss1 = np.append(rmss1, allrmss1)
         allrmsx1 = np.append(rmsx1, allrmsx1)
@@ -63,39 +103,60 @@ for fn in baseFN:
         allrmss2 = np.append(rmss2, allrmss2)
         allrmsx2 = np.append(rmsx2, allrmsx2)
 
+        #Before structure
+        allemx3  = np.append(emx3, allemx3)
+        allrmss3 = np.append(rmss3, allrmss3)
+        allrmsx3 = np.append(rmsx3, allrmsx3)
+
+        #after structure
+        allemx4  = np.append(emx4, allemx4)
+        allrmss4 = np.append(rmss4, allrmss4)
+        allrmsx4 = np.append(rmsx4, allrmsx4)
+
         allq1  = np.append(q1, allq1)
         allq2  = np.append(q2, allq2)
         allq3  = np.append(q3, allq3)
         allq4  = np.append(q4, allq4)
-    
+
+
+    #After data for all generations is saved, 
+    #Make a pareto front using all data from opt run.
     #(pfdata1, ind1) = pareto_pts(allrmss1, allemx1)
     #(pfdata2, ind2) = pareto_pts(allrmsx2, allemx2)
-
-    (pfdata3, ind1) = pareto_pts(allrmss2, allemx2)
-    (pfdata4, ind2) = pareto_pts(allrmsx2, allemx2)
+    #(pfdata2, ind2) = pareto_pts(allrmss2, allrmsx2)
     #plt.plot(pfdata2['x'], pfdata2['y'], '-.')
+
+
+    (pfdatax3, indx3) = pareto_pts(allrmss3, allrmsx3)
+    #plt.plot(pfdata3['x'], pfdata3['y'], '-o')
+
+    #(pfdatay4, indy4) = pareto_pts(allrmss4, allrmsy4)
+    (pfdatax4, indx4) = pareto_pts(allrmss4, allrmsx4)
+    #plt.plot(pfdatax4['x'], pfdatax4['y'], '-o')
     #plt.show()
 
-    print('Q1', allq1[ind2])
-    print('Q2', allq2[ind2])
-    print('Q3', allq3[ind2])
-    print('Q4', allq4[ind2])
-    print('xrms1', allrmsx1[ind1])
-    print('xrms2', pfdata4['x'])
-    print('rmss2', pfdata3['x'])
-    #print(len(data))
-    #gens = ds.getLabel(data)
-    #print(gens)
-    #print(data)
-    #ds.design_variables()
-    #num = getattr(ds, "num_generations")
-    #print(num)
-    #ds.getData('objective or design variable', gen=gen)
 
-    #allxy = dsets.get_all_data()
-    #print(allxy)
+    #print('indy', indy4)
+    print('indx3', indx3)
+    print('indx4', indx4)
+    #Printing design variables that 
+    #correspond to pareto front points
+    print('Q1', allq1[indy4])
+    print('Q2', allq2[indy4])
+    print('Q3', allq3[indy4])
+    print('Q4', allq4[indy4])
+    print('Q5', allq1[indy4])
+    print('Q6', allq2[indy4])
+    print('Q7', allq3[indy4])
+    print('Q8', allq4[indy4])
+
+
+
+    #print('xrms1', allrmsx1[ind1])
+    #print('xrms2', pfdata4['x'])
+    #print('rmss2', pfdata3['x'])
     
+    #gens = ds.getLabel(data)
     #ds = dsets[0]
     #plot_parallel_coordinates(dsets[0], 1)
-    #print(dsets)
 
