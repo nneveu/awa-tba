d07ce53b713b2b5fe61b81d381a98f5ff08cc1f7
nicole <nneveu@hawk.iit.edu>
2018-05-31 23:10:32 -0500

    plots for friday meeting

--- whole_line/whole_pareto.py
+++ whole_line/whole_pareto.py
@@ -2,7 +2,6 @@ import glob
 import numpy as np
 from db import mldb
 import matplotlib.pyplot as plt
-
 #Functions from pyOPALTools
 #https://gitlab.psi.ch/OPAL/pyOPALTools
 from opal.analysis.pareto_fronts import *
@@ -17,191 +16,233 @@ def plot_stuff(title, xname, yname, xyrange=False):
     plt.ylabel(yname, size=20)
     if xyrange:
         plt.axis(xyrange)
-
-
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif') 
 plt.rc('axes', labelsize=18)
 
-root = '/home/nicole/Documents/awa-tba/'
-
 #baseFN = ['./noquads']#['optLinac_40nC']
 #baseFN = ['./noquads/corrected/']
-baseFN = ['./extquads', './noquads']
-
+#baseFN = ['./extquads', './noquads']
+#baseFN = ['to_q7-9']
+z1=True; z2=True; z3=True; z4=False
+gun     = True
+quads   = True
+doublet = True
+triplet = False
+baseFN  = ['./to_q7-9']
+savefile = 'pareto_front_linac2'
+plottitle = 'Pareto Front Before Triplet \n $\sigma_{x,y}$ vs. $\sigma_z$'
+xtitle = 'Bunch Length: $\sigma_z$ [mm]'
+ytitle =  'Beam Size: $\sigma_x$ [mm]'
 for fn in baseFN:
-    allemx1  = []
-    allrmss1 = []
-    allrmsx1 = []
-    allde1   = [] 
+    
+    if gun == True:
+        allphs  = []
+        allfwhm = []
+        allim   = []
+        allib   = []
+    if quads == True:
+        allq1   = []
+        allq2   = [] 
+        allq3   = []
+        allq4   = []
+    if doublet == True:
+        allq5   = []
+        allq6   = []
+    if triplet==True:
+        allq7   = []
+        allq8   = []
+        allq9   = []
+    if z1 == True:
+        allrmss1 = []
+        allrmsx1 = []
+        allrmsy1 = []
+        allrmspx1 = []
+        allrmspy1 = []
+        allde1   = [] 
+    if z2 == True:
+        allrmss2 = []
+        allrmsx2 = []
+        allrmsy2 = []
+        allrmspx2 = []
+        allrmspy2 = []
+        allde2   = [] 
 
-    allemx2  = []
-    allrmss2 = []
-    allrmsx2 = []
+    if z3 ==True:
+        allrmss3 = []
+        allrmsx3 = []
+        allrmsy3 = []
+        allrmspx3 = []
+        allrmspy3 = []
+        allde3   = [] 
 
-    allde3   = []
-    allemx3  = []
-    allrmss3 = []
-    allrmsx3 = []
-   
-    allemx4  = []
-    allrmss4 = []
-    allrmsx4 = []
-     
-    allq1   = []
-    allq2   = [] 
-    allq3   = []
-    allq4   = []
-    allq5   = []
-    allq6   = [] 
-    allq7   = []
-    allq8   = []
-    allq9   = []
-
-    allphs  = []
-    allfwhm = []
-    allim   = []
-    allib   = []
+    if z4==True:
+        allrmss4 = []
+        allrmsx4 = []
+        allrmsy4 = []
+        allrmspx4 = []
+        allrmspy4 = []
+        allde4   = [] 
+ 
 
     ngen  = len(glob.glob(fn+'/results/*.json'))
     dsets = load_dataset(fn+'/results/', ftype=FileType.OPTIMIZER)
     ds    = dsets[0]
 
     for i in range(1,ngen+1):
-        #Loadig all data in the i-th generation
-        de    = ds.getData('DE1', gen=i)
-        emx1  = ds.getData('EMITX1', gen=i)
-        rmss1 = ds.getData('RMSS1', gen=i)
-        rmsx1 = ds.getData('RMSX1', gen=i)
-        
-        rmss2 = ds.getData('RMSS2', gen=i)
-        rmsx2 = ds.getData('RMSX2', gen=i)
-        emx2  = ds.getData('EMITX2', gen=i)
 
-        de3    = ds.getData('DE3', gen=i)
-        emx3  = ds.getData('EMITX3', gen=i)
-        rmss3 = ds.getData('RMSS3', gen=i)
-        rmsx3 = ds.getData('RMSX3', gen=i)
+        if z1==True:
+            #Loadig all data in the i-th generation
+            de1   = ds.getData('DE1', gen=i)
+            rmss1 = ds.getData('RMSS1', gen=i)
+            rmsx1 = ds.getData('RMSX1', gen=i)
+            rmsy1 = ds.getData('RMSY1', gen=i)
 
-        emx4  = ds.getData('EMITX4', gen=i)
-        rmss4 = ds.getData('RMSS4', gen=i)
-        rmsx4 = ds.getData('RMSX4', gen=i)
-        #rmsy4 = ds.getData('RMSY4', gen=i)
+            #Saving i-th generation data to an array
+            #that holds data for every generation
+            #allemx1  = np.append(emx1, allemx1)
+            allrmss1 = np.append(rmss1, allrmss1)
+            allrmsx1 = np.append(rmsx1, allrmsx1)
+            allrmsy1 = np.append(rmsy1, allrmsy1)
+            allde1 = np.append(de1, allde1)
 
-        gphs = ds.getData('GPHASE', gen=i)
-        fwhm = ds.getData('FWHM', gen=i)
-        im = ds.getData('IM', gen=i)
-        ib = ds.getData('IBF', gen=i)
-        q1 = ds.getData('KQ1', gen=i)
-        q2 = ds.getData('KQ2', gen=i)
-        q3 = ds.getData('KQ3', gen=i)
-        q4 = ds.getData('KQ4', gen=i)
-        try:
+        if z2==True:
+            #Loadig all data in the i-th generation
+            de2   = ds.getData('DE2', gen=i)
+            rmss2 = ds.getData('RMSS2', gen=i)
+            rmsx2 = ds.getData('RMSX2', gen=i)
+            rmsy2 = ds.getData('RMSY2', gen=i)
+
+            #Saving i-th generation data to an array
+            #that holds data for every generation
+            #allemx1  = np.append(emx1, allemx1)
+            allrmss2 = np.append(rmss2, allrmss2)
+            allrmsx2 = np.append(rmsx2, allrmsx2)
+            allrmsy2 = np.append(rmsy2, allrmsy2)
+            allde2 = np.append(de2, allde2)
+
+
+        if z3==True:
+            #Loadig all data in the i-th generation
+            de3   = ds.getData('DE2', gen=i)
+            rmss3 = ds.getData('RMSS2', gen=i)
+            rmsx3 = ds.getData('RMSX2', gen=i)
+            rmsy3 = ds.getData('RMSY2', gen=i)
+
+            #Saving i-th generation data to an array
+            #that holds data for every generation
+            #allemx1  = np.append(emx1, allemx1)
+            allrmss3 = np.append(rmss3, allrmss3)
+            allrmsx3 = np.append(rmsx3, allrmsx3)
+            allrmsy3 = np.append(rmsy3, allrmsy3)
+            allde3 = np.append(de3, allde3)
+
+
+        if gun == True:
+            #Some runs don't have q5 and q6
+            gphs = ds.getData('GPHASE', gen=i)
+            fwhm = ds.getData('FWHM', gen=i)
+            im = ds.getData('IM', gen=i)
+            ib = ds.getData('IBF', gen=i)
+
+            allphs  = np.append(gphs, allphs)
+            allfwhm = np.append(fwhm, allfwhm) 
+            allim   = np.append(im, allim)
+            allib   = np.append(ib, allib)
+
+        if quads==True: 
+            q1 = ds.getData('KQ1', gen=i)
+            q2 = ds.getData('KQ2', gen=i)
+            q3 = ds.getData('KQ3', gen=i)
+            q4 = ds.getData('KQ4', gen=i)
+            
+            allq1  = np.append(q1, allq1)
+            allq2  = np.append(q2, allq2)
+            allq3  = np.append(q3, allq3)
+            allq4  = np.append(q4, allq4)
+
+
+        if doublet==True:
             q5 = ds.getData('KQ5', gen=i)
             q6 = ds.getData('KQ6', gen=i)
-        except:
-            pass
-        q7 = ds.getData('KQ7', gen=i)
-        q8 = ds.getData('KQ8', gen=i)
-        q9 = ds.getData('KQ9', gen=i)
 
 
-        #Some runs don't have q5 and q6
-        try:
-            q5 = ds.getData('KQ5', gen=i)
-            q6 = ds.getData('KQ6', gen=i)
-        except:
-            pass
-            #print('no q5, q6')
-       
-        try: 
-            q7 = ds.getData('KQ7', gen=i)
-            q8 = ds.getData('KQ8', gen=i)
-            q9 = ds.getData('KQ9', gen=i)
-        except:
-            pass
-            #print('no q7-q9')
+            #q7 = ds.getData('KQ7', gen=i)
+            #q8 = ds.getData('KQ8', gen=i)
+            #q9 = ds.getData('KQ9', gen=i)
 
-        #Saving i-th generation data to an array
-        #that holds data for every generation
-        allemx1  = np.append(emx1, allemx1)
-        allrmss1 = np.append(rmss1, allrmss1)
-        allrmsx1 = np.append(rmsx1, allrmsx1)
 
-        allemx2  = np.append(emx2, allemx2)
-        allrmss2 = np.append(rmss2, allrmss2)
-        allrmsx2 = np.append(rmsx2, allrmsx2)
+            allq5  = np.append(q5, allq5)
+            allq6  = np.append(q6, allq6)
+            #allq7  = np.append(q7, allq7)
+            #allq8  = np.append(q8, allq8)
+            #allq9  = np.append(q9, allq9)
 
-        #Before structure
-        allemx3  = np.append(emx3, allemx3)
-        allrmss3 = np.append(rmss3, allrmss3)
-        allrmsx3 = np.append(rmsx3, allrmsx3)
 
-        #after structure
-        allemx4  = np.append(emx4, allemx4)
-        allrmss4 = np.append(rmss4, allrmss4)
-        allrmsx4 = np.append(rmsx4, allrmsx4)
-
-        allq1  = np.append(q1, allq1)
-        allq2  = np.append(q2, allq2)
-        allq3  = np.append(q3, allq3)
-        allq4  = np.append(q4, allq4)
-        allq5  = np.append(q5, allq5)
-        allq6  = np.append(q6, allq6)
-        allq7  = np.append(q7, allq7)
-        allq8  = np.append(q8, allq8)
-        allq9  = np.append(q9, allq9)
-
-        allphs  = np.append(gphs, allphs)
-        allfwhm = np.append(fwhm, allfwhm) 
-        allim   = np.append(im, allim)
-        allib   = np.append(ib, allib)
 
     #After data for all generations is saved, 
     #Make a pareto front using all data from opt run.
     #(pfdata1, ind1) = pareto_pts(allrmss1, allemx1)
-    #(pfdata2, ind2) = pareto_pts(allrmsx2, allemx2)
-    #(pfdata2, ind2) = pareto_pts(allrmss2, allrmsx2)
+    #(pfdata2, ind) = pareto_pts(allrmss2, allrmsx2)
+    #(pfdata2, ind) = pareto_pts(allrmss2, allrmsx2)
     #plt.plot(pfdata2['x'], pfdata2['y'], '-.')
-
-    (pfdatax3, indx3) = pareto_pts(allrmss3, allrmsx3)
+    #(pfdataxy, ind) = pareto_pts(allrmsx1, allrmsy1)
+    (pfdatasx, ind) = pareto_pts(allrmss3, allrmsx3)
+    #(pfdatase, ind) = pareto_pts(allrmss1, allde1)
     #plt.plot(pfdata3['x'], pfdata3['y'], '-o')
 
     #(pfdatay4, indy4) = pareto_pts(allrmss4, allrmsy4)
-    (pfdatax4, indx4) = pareto_pts(allrmss4, allrmsx4)
-    plot_stuff('Pareto Front after Structure: \n $\sigma_x$ vs. $\sigma_z$', 'Bunch Length: $\sigma_z$ [mm]', 'Beam Size: $\sigma_x$ [mm]')
-    label = fn.split('./')[1]
-    label = label.split('/')[0]
-    mmxrms = np.asarray(pfdatax4['y'])*10**3
-    mmzrms = np.asarray(pfdatax4['x'])*10**3
+    #(pfdatax4, indx4) = pareto_pts(allrmss4, allrmsx4)
+    plot_stuff(plottitle, xtitle,ytitle)
+    #label = label.split('/')[0]
+    mmyrms = np.asarray(allrmsy3[ind])*10**3
+    mmxrms = np.asarray(pfdatasx['y'])*10**3
+    mmzrms = np.asarray(pfdatasx['x'])*10**3
     #plt.plot(pfdatax4['x'], pfdatax4['y'], '-o', label=fn)
-    plt.plot(mmzrms, mmxrms, '-o', label=fn)
+    plt.plot(mmzrms, mmyrms, '-o', label='yrms')
+    plt.plot(mmzrms, mmxrms, '-o', label='xrms')
     #plt.show()
 
-    #print('indx3', indx3)
     #print('indx4', indx4)
     #Printing design variables that 
     #correspond to pareto front points
     print(fn)
-    print('xrms', pfdatax4['y'])
-    print('zrms', pfdatax4['x'])
-    print('GPHASE', allphs[indx4])
-    print('FWHM', allfwhm[indx4])
-    print('IM', allim[indx4])
-    print('BF', allib[indx4])
-    print('Q1', allq1[indx4])
-    print('Q2', allq2[indx4])
-    print('Q3', allq3[indx4])
-    print('Q4', allq4[indx4])
+#    print('px', allrmspx[ind])
+#    print('py', allrmspy[ind])
+
+
+
+    if z1==True:
+      print('dE1', allde1[ind])
+      print('yrms1', allrmsy1[ind])
+      print('xrms1', allrmsx1[ind])
+      print('zrms1', allrmss1[ind])    
+    
+    
+    
+    if z3==True:  
+        print('dE', allde2[ind])
+        print('yrms', allrmsy2[ind])
+        print('xrms', allrmsx2[ind])
+        print('zrms', allrmss1[ind])
+
+    print('GPHASE', allphs[ind])
+    print('FWHM', allfwhm[ind])
+    print('IM', allim[ind])
+    print('BF', allib[ind])
+    print('Q1', allq1[ind])
+    print('Q2', allq2[ind])
+    print('Q3', allq3[ind])
+    print('Q4', allq4[ind])
     try:
-        print('Q5', allq5[indx4])
-        print('Q6', allq6[indx4])
+        print('Q5', allq5[ind])
+        print('Q6', allq6[ind])
+    #    print('Q7', allq7[ind])
+    #    print('Q8', allq8[ind])
+    #    print('Q9', allq9[ind])
+    #
     except:
         pass
-    print('Q7', allq7[indx4])
-    print('Q8', allq8[indx4])
-    print('Q9', allq9[indx4])
-
     #print('xrms1', allrmsx1[ind1])
     #print('xrms2', pfdata4['x'])
     #print('rmss2', pfdata3['x'])
@@ -212,4 +253,4 @@ for fn in baseFN:
 #plt.plot([0,5], [3,3], 'k-')
 plt.grid()
 plt.legend()
-plt.savefig('quads_noquads.pdf', dpi=1000, bbox_inches='tight')
+plt.savefig(savefile, dpi=1000, bbox_inches='tight')
