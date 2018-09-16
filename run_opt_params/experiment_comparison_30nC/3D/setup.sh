8d50221ca0d111e5f5f4f25c642039b60fff08c0
Nicole Neveu <nneveu@hawk.iit.edu>
2018-09-16 03:40:58 -0500

    opal input files for experimental comparison

---
+++ run_opt_params/experiment_comparison_30nC/3D/setup.sh
@@ -0,0 +1,9 @@
+# general of opal
+#rm -rf results *.0 *_0
+#rm -rf tmp
+#mkdir tmp results
+export QUEUE=knlall
+export TEMPLATES=`pwd`/tmpl/
+export TIME=01:00:00
+export OPAL_EXE_PATH=/lcrc/project/AWA-beam-dynamics/software/knl/bin
+export FIELDMAPS=/lcrc/project/AWA-beam-dynamics/FieldFiles/DriveFiles
